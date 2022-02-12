ed0654b6a15911da4f1f91d2f120e56a3dc37bc8
Rion <rionfx.42@gmail.com>
2022-02-12 16:26:44 +0900

    first commit

---
+++ AnimationWave/AnimationWaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationWaveApp.swift
+//  AnimationWave
+//
+//  Created by FX on 2022/02/12.
+//
+
+import SwiftUI
+
+@main
+struct AnimationWaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
